2e7041efe1a3ed2dc9bc0997e09186f6a99c3cc1
l0rdg3x <l0rdg3x@gmail.com>
2017-10-04 13:24:24 +0200

    do_snapshot: Add various check Prevent bug (snapshot in other snapshot if create 2nd snapshot in same minute) Check if MOUNT_ROOT exist Check if MOUNT_ROOT is empty

--- do_snapshot.sh
+++ do_snapshot.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 
 BTRFS_ROOT=/dev/sdb3
+MOUNT_ROOT=/mnt/root
 
-sudo mount $BTRFS_ROOT /mnt/root
-cd /mnt/root/
-sudo btrfs subvolume snapshot ./@ ./@$(date +%F_%H-%M)
-sudo btrfs subvolume snapshot ./@home ./@home$(date +%F_%H-%M)
+if [ ! -d "$MOUNT_ROOT" ]; then
+    # On first execute, create MOUNT_ROOT dir
+    sudo mkdir $MOUNT_ROOT
+else
+    echo "Skip creation of root folder"
+fi
+
+if [ ! "$(ls -A "$MOUNT_ROOT")" ]; then
+    # If MOUNT_ROOT is empty then mount BTRFS_ROOT in MOUNT_ROOT
+    sudo mount $BTRFS_ROOT $MOUNT_ROOT
+else
+    # Skip mount
+    echo "Skip mount"
+fi
+
+cd $MOUNT_ROOT
+
+if [ ! -d "@$(date +%F_%H-%M)" ]; then
+    sudo btrfs subvolume snapshot ./@ ./@$(date +%F_%H-%M)
+else
+    # Already exist a snapshot with same date and time
+    echo "Skip root snapshot"
+fi
+
+if [ ! -d "@home$(date +%F_%H-%M)" ]; then
+    sudo btrfs subvolume snapshot ./@home ./@home$(date +%F_%H-%M)
+else
+    # Already exist a snapshot with same date and time
+    echo "Skip home snapshot"
+fi
+
 cd ~
 sleep 10
-sudo umount /mnt/root
+sudo umount $MOUNT_ROOT
